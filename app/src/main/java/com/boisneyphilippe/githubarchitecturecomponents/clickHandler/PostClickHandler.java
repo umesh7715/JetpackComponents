c072aff143fbfd25ce586a663000b7a0cb28975b
UMESH SARAVANE <harjai.umesh@angelbroking.com>
2018-06-30 10:47:56 +0530

    Data Binding

--- app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/clickHandler/PostClickHandler.java
+++ app/src/main/java/com/boisneyphilippe/githubarchitecturecomponents/clickHandler/PostClickHandler.java
@@ -1,5 +1,6 @@
 package com.boisneyphilippe.githubarchitecturecomponents.clickHandler;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import com.boisneyphilippe.githubarchitecturecomponents.database.entity.Post;
 public class PostClickHandler {
 
     Context context;
+    Activity activity;
 
     public PostClickHandler(Context context) {
         this.context = context;
